ab13f39227827cdb6b2ba0b10e85f96a79494398
Anatoly Leskovets <ableskovets@gmail.com>
2016-10-30 02:50:22 +0300

    Json visibility checker

--- src/main/java/ru/skuptsov/telegram/bot/platform/config/TelegramBotClientConfiguration.java
+++ src/main/java/ru/skuptsov/telegram/bot/platform/config/TelegramBotClientConfiguration.java
@@ -1,6 +1,9 @@
 package ru.skuptsov.telegram.bot.platform.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
 import com.ning.http.client.AsyncHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +24,11 @@ public class TelegramBotClientConfiguration {
     private String baseUrl;
 
     public ObjectMapper getObjectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setVisibilityChecker(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+
+        return objectMapper;
     }
 
     public TelegramBotHttpClient createTelegramBotClient(AsyncHttpClient asyncHttpClient) {
